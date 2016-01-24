446c2b2a8e3d814850decb74f8ffcc7d4520a6b4
Derek <saif.tareq@gmail.com>
2016-01-24 08:16:34 -0800

    Joystick registers the basic inputs and directions

--- src/joystick.cpp
+++ src/joystick.cpp
@@ -7,34 +7,106 @@ Joystick::Joystick(unsigned sdl_joy_number){
   raw_buffer_length = SDL_JoystickNumButtons(device);
   current_buffer = new Uint8[raw_buffer_length];
   previous_buffer = new Uint8[raw_buffer_length];
-  buffer = new Button[buffer_size];
+  pressed_buffer = new Uint8[bEND];
   for(unsigned i=0; i<buffer_size; i++){
-    buffer[i] = bNULL;
+    buffer.push_back(bNULL);
   }
   for(int i=0; i<raw_buffer_length; i++){
     current_buffer[i] = 0;
     previous_buffer[i] = 0;
   }
+  for(unsigned i=0; i<bEND; i++){
+    pressed_buffer[i] = bNULL;
+  }
+  mapping = new Button[100]; //Supports a gamepad with up to 100 buttons
+  setDefaultButtonMappings();
 }
 
 Joystick::~Joystick(){
-  delete buffer;
+  delete mapping;
+  delete pressed_buffer;
   delete previous_buffer;
   delete current_buffer;
   SDL_JoystickClose(device);
 }
 
+Button Joystick::sdlDirToButtonDir(Uint8 dir){
+  switch(dir){
+    case SDL_HAT_UP:
+      return bUP;
+    case SDL_HAT_DOWN:
+      return bDOWN;
+    case SDL_HAT_LEFT:
+      return bLEFT;
+    case SDL_HAT_RIGHT:
+      return bRIGHT;
+    case SDL_HAT_RIGHTUP:
+      return bUPRIGHT;
+    case SDL_HAT_RIGHTDOWN:
+      return bDOWNRIGHT;
+    case SDL_HAT_LEFTUP:
+      return bUPLEFT;
+    case SDL_HAT_LEFTDOWN:
+      return bDOWNLEFT;
+  }
+  return bNULL;
+}
+
 void Joystick::update(){
+  bool change = false;
+
+  // Handle directional buttons
+  previous_hat = current_hat;
+  current_hat = SDL_JoystickGetHat(device, 0);
+  if (current_hat != SDL_HAT_CENTERED && previous_hat != current_hat){
+    buffer.erase(buffer.begin());
+    buffer.push_back(sdlDirToButtonDir(current_hat));
+    change = true;
+  }
+
+  // Handle nondirectional buttons
   for(int i=0; i<raw_buffer_length; i++){
     previous_buffer[i] = current_buffer[i];
     current_buffer[i] = SDL_JoystickGetButton(device, i);
-
     if (!previous_buffer[i] & current_buffer[i]){
-      cout << "Pressed: " << i << endl;
+      pressed_buffer[mapping[i]] = 1;
+      buffer.erase(buffer.begin());
+      buffer.push_back(mapping[i]);
+      change = true;
     }
+  }
+  /*
+   * Check for combinations here and mark them manually
+   */
+  if (change){
+    dumpBuffer();
   }
 }
 
 Button Joystick::lastPressed(){
   return buffer[buffer_size-1];
+}
+
+void Joystick::setDefaultButtonMappings(){
+  mapping[0] = bMP;
+  mapping[1] = bMK;
+  mapping[2] = bLK;
+  mapping[3] = bLP;
+  mapping[4] = bTK;
+  mapping[5] = bHK;
+  mapping[6] = bTP;
+  mapping[7] = bHP;
+  mapping[8] = bSELECT;
+  mapping[9] = bSTART;
+}
+
+void Joystick::dumpBuffer(){
+  cout << "[";
+  for(unsigned i=0; i<buffer.size(); i++){
+    cout << legible_buttons[buffer[i]];
+    if (i != buffer.size()-1){
+      cout << ", ";
+    }
+  }
+  cout << "]" << endl;
 }
--- src/joystick.h
+++ src/joystick.h
@@ -1,6 +1,10 @@
 #ifndef JOYSTICK_H
 #define JOYSTICK_H
+
+#include <string>
+#include <vector>
 #include <SDL.h>
+using namespace std;
 
 enum Button {
   bNULL,
@@ -23,23 +27,58 @@ enum Button {
   bHK,
   bTK,
   bOVERHEAD,
-  bTAUNT
+  bTAUNT,
+  bHARDRESET,
+  bEND
+};
+
+const string legible_buttons[] = {
+  "  ",
+  "↑ ",
+  "↓ ",
+  "← ",
+  "→ ",
+  "↖ ",
+  "↗ ",
+  "↙ ",
+  "↘ ",
+  "S ",
+  "s ",
+  "□ ",
+  "△ ",
+  "R1",
+  "L1",
+  "X ",
+  "O ",
+  "R2",
+  "L2",
+  "OV",
+  "TA",
+  "HR",
+  "EN"
 };
 
 class Joystick {
   private:
     SDL_Joystick* device;
     const unsigned buffer_size = 20;
-    Button* buffer = NULL;
+    vector<Button> buffer;
     int raw_buffer_length = 0;
     Uint8* current_buffer = NULL;
     Uint8* previous_buffer = NULL;
+    Uint8 current_hat = 0;
+    Uint8 previous_hat = 0;
+    Uint8* pressed_buffer = NULL;
+    Button* mapping = NULL;
+    Button sdlDirToButtonDir(Uint8);
 
   public:
     Joystick(unsigned);
     ~Joystick();
     void update();
     Button lastPressed();
+    void setDefaultButtonMappings();
+    void dumpBuffer();
 };
 
 #endif
