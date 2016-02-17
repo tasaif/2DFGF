435702a5323bdcdec9b4021bec314c3b58b75496
Derek <saif.tareq@gmail.com>
2016-02-16 18:46:54 -0800

    Remove old move code

--- src/moves/null.cpp
+++ src/moves/null.cpp
@@ -19,29 +19,4 @@ void NullMoveSystem::initCharacterMoves(){
 
 bool NullMoveSystem::checkForMove(){
   return MoveSystem::checkForMove();
-  /*switch(f->getState())
-    case psWALKF:
-    case psWALKB:
-    case psNEUTRAL:
-      if (p->joystick->Pressed(bHP)){
-        type = psNORMAL;
-        number = nunHP;
-        f->setSprite(c->getNormAnim(number)->primeSprite());
-        return true;
-      } else if (p->joystick->Pressed(bLP)){
-        type = psNORMAL;
-        number = nunLP;
-        f->setSprite(c->getNormAnim(number)->primeSprite());
-        return true;
-      } else if (p->joystick->Pressed(bMP)){
-        type = psSPECIAL;
-        number = nusFIREBALL;
-        f->setSprite(c->getSpecAnim(number)->primeSprite());
-        return true;
-      }
-      break;
-    default:
-      break;
-  }*/
-  return false;
 }
